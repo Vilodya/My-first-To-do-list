bb7aed314bcb6c1f02da538cdf8c2aa4f48f8e48
Vilodya <rygyj95@gmail.com>
2025-01-09 22:33:23 +0500

    Сделал переключение темы

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,13 @@
-function updateImagesBasedOnTheme() {
-  const themeImage = document.querySelector('.empty__image');
-  const buttonImage = document.querySelector('.theme-button__image');
+const htmlElement = document.documentElement;
+const header = document.querySelector('.header');
+const main = document.querySelector('.main');
+const empty = main.querySelector('.empty');
 
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    buttonImage.src = './svg/sun-icon.svg';
-    themeImage.src = './svg/detective-image-dark.svg';
-  } else {
-    buttonImage.src = './svg/moon-icon.svg';
-    themeImage.src = './svg/detective-image-light.svg';
-  }
-}
+const themeToggleButton = header.querySelector('.theme-button');
+const deleteButton = main.querySelector('.delete-button');
 
-updateImagesBasedOnTheme();
+const themeImage = document.querySelector('.empty__image');
+const buttonImage = document.querySelector('.theme-button__image');
 
 function updateDate() {
   const months = [
@@ -29,6 +25,32 @@ function updateDate() {
 
 updateDate();
 
-function addTask() {
-  
+themeToggleButton.addEventListener('click', () => {
+  htmlElement.classList.toggle('dark');
+
+  if (htmlElement.classList.contains('dark')) {
+    localStorage.setItem('theme', 'dark');
+    buttonImage.src = './svg/sun-icon.svg';
+    themeImage.src = './svg/detective-image-dark.svg';
+  } else {
+    localStorage.setItem('theme', 'light');
+    buttonImage.src = './svg/moon-icon.svg';
+    themeImage.src = './svg/detective-image-light.svg';
+  }
+});
+
+function listHasTasks() {
+  empty.classList.add('no-songs_hidden');
 }
+
+function listNoTasks() {
+  empty.classList.remove('no-songs_hidden');
+}
+
+// function addTask() {
+  
+// }
+
+// deleteButton.addEventListener('click', function () {
+//   const task 
+// });
